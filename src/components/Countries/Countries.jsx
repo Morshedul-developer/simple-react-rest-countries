8ed093d201fa87948e8e110a5b4f7799347bde61
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-19 17:21:53 +0600

    explored more about react lift up state

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,14 +5,20 @@ import "./Countries.css";
 const Countries = ({ messageFetch }) => {
   const [visitedCountries, setVisitedCountries] = useState([]);
   const handleVisitedCountry = (country) => {
-    console.log("Clicked", country);
+    const newArr = [...visitedCountries, country];
+    setVisitedCountries(newArr);
   };
   const allCountries = use(messageFetch);
   const countries = allCountries.countries;
   return (
     <div>
       <h1>In the countries: {countries.length}</h1>
-      <h2>Total visited countries: </h2>
+      <h2>Total visited countries: {visitedCountries.length}</h2>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li key={country.cca3.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
           <Country 
