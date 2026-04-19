86d8eb2ebb9e1bf28bf5edf1236235d003bd6f33
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-19 15:36:06 +0600

    added functionality to the visit button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     // console.log(country);
+    const [visited, setVisited] = useState(false);
+    const handleVisit = () => {
+        // system 1
+        // setVisited(visited ? false : true);
+        // system 2
+        setVisited(!visited);
+    }
     return (
-        <div className='border'>
+        <div className={`border ${visited && "bg"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? "(Large)" : "(Small)"}</p>
+            <button onClick={handleVisit}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
