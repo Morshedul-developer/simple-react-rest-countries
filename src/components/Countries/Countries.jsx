a97265c0306e0f863052008f3b86ec925fc07a73
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-19 17:44:06 +0600

    made a visit flag button and also made it functional

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,27 +4,39 @@ import "./Countries.css";
 
 const Countries = ({ messageFetch }) => {
   const [visitedCountries, setVisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
+
   const handleVisitedCountry = (country) => {
     const newArr = [...visitedCountries, country];
     setVisitedCountries(newArr);
   };
+  const handleVisitedFlag = (flag) => {
+    const newArr = [...visitedFlags, flag];
+    setVisitedFlags(newArr);
+  }
   const allCountries = use(messageFetch);
   const countries = allCountries.countries;
   return (
     <div>
       <h1>In the countries: {countries.length}</h1>
       <h2>Total visited countries: {visitedCountries.length}</h2>
+      <h3>Total visited flags: {visitedFlags.length}</h3>
       <ol>
         {visitedCountries.map((country) => (
           <li key={country.cca3.cca3}>{country.name.common}</li>
         ))}
+      </ol>
+      <ol>
+        {visitedFlags.map((flag, index) => <li key={index}><img src={flag} alt="Flag" /></li>)}
       </ol>
       <div className="countries">
         {countries.map((country) => (
           <Country 
           key={country.cca3.cca3} 
           country={country}
-          handleVisitedCountry = {handleVisitedCountry}></Country>
+          handleVisitedCountry = {handleVisitedCountry}
+          handleVisitedFlag = {handleVisitedFlag}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country, handleVisitedCountry}) => {
+const Country = ({country, handleVisitedCountry, handleVisitedFlag}) => {
     // console.log(country);
     const [visited, setVisited] = useState(false);
     const handleVisit = () => {
@@ -16,8 +16,16 @@ const Country = ({country, handleVisitedCountry}) => {
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? "(Large)" : "(Small)"}</p>
-            <button onClick={handleVisit}>
+            <button style={{
+                marginRight: "5px",
+                cursor: "pointer"
+            }} onClick={handleVisit}>
                 {visited ? "Visited" : "Not Visited"}
+            </button>
+            <button style={{
+                cursor: "pointer"
+            }} onClick={() => handleVisitedFlag(country.flags.flags.png)}>
+                Visit Flag
             </button>
         </div>
     );
